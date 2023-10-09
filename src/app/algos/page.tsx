a47acbf700ca82b681df29970cc3da51103700a9
Jeffrey Meng <jeffreymeng@users.noreply.github.com>
2023-10-09 13:55:31 -0700

    fix

--- src/app/algos/page.tsx
+++ src/app/algos/page.tsx
@@ -5,7 +5,7 @@ export default function AlgosPage() {
   return (
     <Page title={"Algorithms"}>
       <p className="mt-6 text-lg leading-5 text-gray-900 dark:text-white">
-        This is a working list of a few data structures that I've implemented,
+        This is a working list of a few data structures that I&apos;ve implemented,
         typically in the course of interview prep.
       </p>
     </Page>
