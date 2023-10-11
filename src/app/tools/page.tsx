173f2322fd7d6150c4791f197c58dc3d8f0f1329
Jeffrey Meng <jeffkmeng@gmail.com>
2023-10-11 11:35:43 -0700

    fix: builds

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -6,8 +6,8 @@ export default function ToolsPage() {
   return (
     <Page title={"Tools"}>
       <p className="mt-6 text-lg leading-5 text-gray-900 dark:text-white">
-        This section contains some utilities that I've made, mostly for my own
-        convenience.
+        This section contains some utilities that I&apos;ve made, mostly for my
+        own convenience.
       </p>
       <ul className="list-disc mt-6">
         <li>
