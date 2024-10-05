b3f6cac7ec3eaac1d8676be74430c88339678756
Jeffrey Meng <jeffkmeng@gmail.com>
2024-10-05 14:25:03 -0400

    use different parallax method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,6 @@ import socials from "@/data/socials";
 import Project from "@/components/Project";
 import Link from "next/link";
 import Image from "next/image";
-import {
-  ParallaxBanner,
-  ParallaxBannerLayer,
-  ParallaxProvider,
-} from "react-scroll-parallax";
 
 function ExternalLink({
   href,
@@ -62,139 +57,150 @@ export default function Home() {
       document.removeEventListener("keypress", handler);
     };
   });
+  const [bgTranslate, setBgTranslate] = useState(0);
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY < window.innerHeight * 0.5) {
+        setBgTranslate(-window.scrollY * 0.75);
+        console.log(window.scrollY);
+      }
+    };
+    document.addEventListener("scroll", handler);
+    return () => {
+      document.removeEventListener("scroll", handler);
+    };
+  }, []);
   return (
     <main className={cx(darkMode && "dark")}>
-      <ParallaxProvider>
-        {/*<div className="w-screen mx-0 px-0 top-0">*/}
-        <ParallaxBanner className="min-h-[22vh] sm:h-[30vh] w-full">
-          <ParallaxBannerLayer
-            image="/header.webp"
-            className={""}
-            speed={-20}
-            expanded={true}
-            translateY={["-200px", "-70px"]}
-          ></ParallaxBannerLayer>
-          <ParallaxBannerLayer
-            className={
-              "relative py-8 sm:py-16 pb-24 mx-auto max-w-7xl px-6 md:px-16 lg:px-36 z-10 opacity-95"
-            }
-          >
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Jeffrey Meng
-            </h2>
-            <p className="mt-6 text-lg leading-6 text-white">
-              I&apos;m a software engineer thinking about <i>Systems</i>,{" "}
-              <i>Programming Languages</i>, and <i>the Web</i>.
-            </p>
-          </ParallaxBannerLayer>
-        </ParallaxBanner>
-        {/*</div>*/}
-        <div
-          className={cx(
-            "relative isolate pt-4 sm:pt-8 pb-12 sm:pb-32 min-h-screen bg-gray-50 [box-shadow:0_0px_16px_0_rgb(0,0,0,0.3)]",
-            darkMode && "bg-gradient-to-br to-slate-900 from-sky-900"
-          )}
-        >
-          <div
-            className={
-              "fixed h-screen w-screen gradient-track m-0 top-0 left-0 z-0"
-            }
-          ></div>
-          <div className="relative mx-auto max-w-7xl px-6 md:px-16 lg:px-36 z-10">
-            <div className="max-w-3xl">
-              <div className="text-base leading-7 text-gray-700 dark:text-gray-300">
-                <p>
-                  I&apos;m an undergraduate studying Computer Science at UC
-                  Irvine, where I also work on compilers-related research with{" "}
-                  <ExternalLink href={"https://michaelfranz.com"}>
-                    Prof. Michael Franz
-                  </ExternalLink>
-                  . My passion is building well-designed applications and
-                  systems that increase efficiency and create exceptional
-                  experiences. During my time off from school, I&apos;ve been
-                  fortunate enough to have the chance to intern at various cool
-                  companies: I&apos;m currently at{" "}
-                  <ExternalLink href={"https://modal.com"}>Modal</ExternalLink>{" "}
-                  this fall, and in Summer 2025, I&apos;ll be at{" "}
-                  <ExternalLink href={"https://janestreet.com"}>
-                    Jane Street
-                  </ExternalLink>
-                  . In previous years, I&apos;ve interned at{" "}
-                  <ExternalLink href={"https://www.databricks.com"}>
-                    Databricks
-                  </ExternalLink>{" "}
-                  and{" "}
-                  <ExternalLink href={"https://www.ibm.com"}>IBM</ExternalLink>.
-                </p>
-                <p className={"mt-8"}>
-                  When I&apos;m not behind a computer, you can find me outdoors
-                  climbing, skiing, backpacking, or diving (depending on the
-                  season), or indoors playing board games.
-                </p>
-              </div>
-            </div>
-            <div className={"mt-12"}>
-              <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-                Projects
-              </h3>
-              <p className="mt-6 text-lg leading-5 text-gray-700 dark:text-gray-300">
-                Here&apos;s a few of the things I&apos;ve been working on
-                recently:
-              </p>
-              <div className="mx-auto mt-6 sm:mt-10 max-w-7xl sm:mt-20 md:mt-12">
-                <div className="grid max-w-2xl grid-cols-1 sm:gap-x-4 gap-y-6 text-base leading-7 text-gray-300 sm:grid-cols-2 sm:-mx-4 lg:max-w-none lg:grid-cols-3">
-                  {projects.map((project) => (
-                    <Project key={project.name} {...project} />
-                  ))}
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-12 sm:mt-8">
-              <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-                Contact Me
-              </h3>
-
-              <div className={"mt-6"}>
-                {socials.map(({ name, href, text, icon: ItemIcon }) =>
-                  React.createElement(
-                    href.startsWith("/") ? Link : "a",
-                    {
-                      key: name,
-                      href,
-                      className:
-                        "block mt-3 dark:text-gray-400 text-gray-500 sm:hover:text-gray-700 dark:sm:hover:text-gray-200 sm:transition-colors text-md",
-                    },
-                    <>
-                      <span className="sr-only">{name}</span>
-                      <ItemIcon
-                        className="h-6 w-6 inline mr-2"
-                        aria-hidden="true"
-                      />
-                      {text}
-                    </>
-                  )
-                )}
-                <p className={"text-gray-500 dark:text-gray-400 mt-8"}>
-                  If you&apos;d like, you can also{" "}
-                  <a
-                    className={
-                      "inline hover:text-cyan-600 dark:hover:text-cyan-500 transition-all underline"
-                    }
-                    href={"/Jeffrey%20Meng.asc"}
-                  >
-                    download my PGP key
-                  </a>
-                  .
-                </p>
-              </div>
-            </div>
-            <p className="mt-16 text-md leading-5 text-gray-600 dark:text-gray-400">
-              Copyright &copy; 2023-{new Date().getFullYear()} Jeffrey Meng
-            </p>
-          </div>
+      {/*<div className="w-screen mx-0 px-0 top-0 ">*/}
+      {/*<div className={"top-0 left-0 mx-0 px-0 w-screen h-[40vh]"}>*/}
+      <img
+        src={"/header.jpg"}
+        alt={"background image of me on a mountain pass"}
+        className={"object-cover fixed h-[30vh] sm:h-[40vh] w-full -z-50"}
+        style={{
+          transform: `translateY(${bgTranslate}px)`,
+        }}
+      />
+      <div
+        className={
+          "relative py-8 sm:py-16 pb-24 mx-auto max-w-7xl px-6 md:px-16 lg:px-36 z-10 h-[30vh] sm:h-[40vh] "
+        }
+      >
+        <div className={"sm:bottom-16 sm:absolute"}>
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Jeffrey Meng
+          </h2>
+          <p className="mt-6 text-lg leading-6 text-white">
+            I&apos;m a software engineer thinking about <i>Systems</i>,{" "}
+            <i>Programming Languages</i>, and <i>the Web</i>.
+          </p>
         </div>
-      </ParallaxProvider>
+      </div>
+      <div
+        className={cx(
+          "relative isolate pt-4 sm:pt-8 pb-12 sm:pb-32 min-h-screen bg-gray-50 [box-shadow:0_0px_16px_0_rgb(0,0,0,0.3)]",
+          darkMode && "bg-gradient-to-br to-slate-900 from-sky-900"
+        )}
+      >
+        <div
+          className={
+            "fixed h-screen w-screen gradient-track m-0 top-0 left-0 z-0"
+          }
+        ></div>
+        <div className="relative mx-auto max-w-7xl px-6 md:px-16 lg:px-36 z-10">
+          <div className="max-w-3xl">
+            <div className="text-base leading-7 text-gray-700 dark:text-gray-300">
+              <p>
+                I&apos;m an undergraduate studying Computer Science at UC
+                Irvine, where I also work on compilers-related research with{" "}
+                <ExternalLink href={"https://michaelfranz.com"}>
+                  Prof. Michael Franz
+                </ExternalLink>
+                . I love deeply understanding systems, writing beautiful code,
+                and creating amazing software. During my time off from school, I
+                greatly enjoy interning at various cool companies: I&apos;m
+                currently at{" "}
+                <ExternalLink href={"https://modal.com"}>Modal</ExternalLink>{" "}
+                this fall, and in Summer 2025, I&apos;ll be at{" "}
+                <ExternalLink href={"https://janestreet.com"}>
+                  Jane Street
+                </ExternalLink>
+                . In previous years, I&apos;ve interned at{" "}
+                <ExternalLink href={"https://www.databricks.com"}>
+                  Databricks
+                </ExternalLink>{" "}
+                and{" "}
+                <ExternalLink href={"https://www.ibm.com"}>IBM</ExternalLink>.
+              </p>
+              <p className={"mt-8"}>
+                When I&apos;m not behind a computer, you can find me outdoors
+                climbing, skiing, backpacking, or diving (depending on the
+                season), or indoors playing board games.
+              </p>
+            </div>
+          </div>
+          <div className={"mt-12"}>
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+              Projects
+            </h3>
+            <p className="mt-6 text-lg leading-5 text-gray-700 dark:text-gray-300">
+              Here&apos;s a few of the things I&apos;ve been working on
+              recently:
+            </p>
+            <div className="mx-auto mt-6 sm:mt-10 max-w-7xl sm:mt-20 md:mt-12">
+              <div className="grid max-w-2xl grid-cols-1 sm:gap-x-4 gap-y-6 text-base leading-7 text-gray-300 sm:grid-cols-2 sm:-mx-4 lg:max-w-none lg:grid-cols-3">
+                {projects.map((project) => (
+                  <Project key={project.name} {...project} />
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-12 sm:mt-8">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+              Contact Me
+            </h3>
+
+            <div className={"mt-6"}>
+              {socials.map(({ name, href, text, icon: ItemIcon }) =>
+                React.createElement(
+                  href.startsWith("/") ? Link : "a",
+                  {
+                    key: name,
+                    href,
+                    className:
+                      "block mt-3 dark:text-gray-400 text-gray-500 sm:hover:text-gray-700 dark:sm:hover:text-gray-200 sm:transition-colors text-md",
+                  },
+                  <>
+                    <span className="sr-only">{name}</span>
+                    <ItemIcon
+                      className="h-6 w-6 inline mr-2"
+                      aria-hidden="true"
+                    />
+                    {text}
+                  </>
+                )
+              )}
+              <p className={"text-gray-500 dark:text-gray-400 mt-8"}>
+                If you&apos;d like, you can also{" "}
+                <a
+                  className={
+                    "inline hover:text-cyan-600 dark:hover:text-cyan-500 transition-all underline"
+                  }
+                  href={"/Jeffrey%20Meng.asc"}
+                >
+                  download my PGP key
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+          <p className="mt-16 text-md leading-5 text-gray-600 dark:text-gray-400">
+            Copyright &copy; 2023-{new Date().getFullYear()} Jeffrey Meng
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/app/split/page.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import React, { ChangeEvent } from "react";
+import Page from "@/components/Page";
+import StyledLink from "@/components/StyledLink";
+
+function Input({
+  label,
+  value,
+  onChange,
+}: {
+  label: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}) {
+  // TODO: use flex
+  return (
+    <div className={"mt-5"}>
+      <label
+        htmlFor={`input-${label}`}
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          type="number"
+          name={`input-${label}`}
+          value={value}
+          onChange={onChange}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+  );
+}
+
+function replaceAtIndex<T>(arr: T[], i: number, val: T): T[] {
+  const dup = arr.slice();
+  dup[i] = val;
+  return dup;
+}
+
+function isZero(x: null | number) {
+  return x === null || x === 0;
+}
+
+function parseMoney(x: string) {
+  const n = parseFloat(x);
+  if (isNaN(n)) {
+    return null;
+  }
+  return Math.round(n * 100);
+}
+
+function formatMoney(x: number | null) {
+  if (x === null) return "";
+  const dollars = Math.floor(x / 100);
+  const cents = ("" + (x % 100)).padEnd(2, "0");
+  return `${dollars}.${cents}`;
+}
+
+const TIMES = "×";
+
+export default function SplitPage() {
+  const [total, setTotal] = React.useState<null | number>(null);
+  const [subtotal, setSubtotal] = React.useState<number | null>(null);
+  const [shared, setShared] = React.useState<number | null>(null);
+  const [people, setPeople] = React.useState<(number | null)[]>([null, null]);
+  console.log(people);
+  return (
+    <Page title={"Fast Check Splitter"}>
+      <p className="mt-6 text-lg leading-5 text-gray-900 dark:text-white">
+        A simple utility for quickly splitting bills, optimized for efficient
+        input on mobile.
+      </p>
+      {/*TODO: store input and money (to handle invalid input, turn red), emit a onChange only on valid input.
+      Call component moneyinput.*/}
+      <div className={"mt-5"}>
+        <Input
+          label={"Total (including tax and tip)"}
+          value={total}
+          onChange={(e) => setTotal(e.target.value)}
+        />
+        <Input
+          label={"Subtotal"}
+          value={subtotal}
+          onChange={(e) => setTotal(e.target.value)}
+        />
+        <Input
+          label={"Shared Items"}
+          value={shared}
+          onChange={(e) => setTotal(e.target.value)}
+        />
+        {people.map((personSubtotal, index) => (
+          <p className={"mt-3"} key={index}>
+            <b>Person {index + 1}</b>:{" "}
+            <input
+              type="number"
+              className="w-20 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={personSubtotal == null ? "" : formatMoney(personSubtotal)}
+              onChange={(e) => {
+                setPeople((o) => {
+                  const arr = replaceAtIndex(
+                    o,
+                    index,
+                    parseMoney(e.target.value)
+                  );
+                  if (
+                    arr.length >= 3 &&
+                    isZero(arr[arr.length - 1]) &&
+                    isZero(arr[arr.length - 2])
+                  ) {
+                    arr.pop();
+                  } else if (!isZero(arr[arr.length - 1])) {
+                    arr.push(null);
+                  }
+                  return arr;
+                });
+              }}
+            />{" "}
+            {TIMES} <span>1.2577</span> ={" "}
+            <span>
+              $
+              {personSubtotal === null
+                ? ""
+                : formatMoney(Math.round(personSubtotal * 1.2577))}
+            </span>
+          </p>
+        ))}
+        {/*  TODO: make the cents add up for the calculation instead of rounding. */}
+      </div>
+    </Page>
+  );
+}
